de30aa6731951fe44b73add91a591252ac5a5f08
Michael Williams <draringi@draringi.net>
2014-10-21 10:51:41 -0400

    Initial receipt work New Dependancy: ReportLab

--- machinerequests/views.py
+++ machinerequests/views.py
@@ -3,6 +3,13 @@ from django.views.generic import ListView, DetailView, CreateView
 from machinerequests.models import Request, Machine
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.http import HttpResponse
+
+from io import BytesIO
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 # Create your views here.
 
@@ -53,3 +60,12 @@ class MachineView(DetailView):
     model = Machine
     context_object_name = 'machine'
     template_name = 'machine_requests/machine_view.html'
+
+
+def generate_receipt(request, pk):
+    machine = get_object_or_404(Machine, pk=pk)
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="somefilename.pdf"'
+    buffer = BytesIO()
+    doc = SimpleDocTemplate(buffer, pagesize=letter, rightMargin=62, leftMargin=62, topMargin=64)
+    template = get_template('machine_requests/receipt_pdf.rml'
